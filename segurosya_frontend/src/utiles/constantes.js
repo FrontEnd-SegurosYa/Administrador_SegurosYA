8d1a7d55c87078befaad557e8aee66b0b3a54988
MarceloGal <69372076+MarceloGal@users.noreply.github.com>
2023-06-22 19:43:36 -0500

    Correccion link

--- segurosya_frontend/src/utiles/constantes.js
+++ segurosya_frontend/src/utiles/constantes.js
@@ -2,6 +2,6 @@ module.exports = {
     //EC2 Bruno
     LINKSERVER: "http://44.198.73.103:8080",
     //EC2 Lennon
-    LINKSERVER: "http://3.89.34.248:8080",
+    // LINKSERVER: "http://3.89.34.248:8080",
     // LINKSERVER: "http://localhost:8080",
 };
